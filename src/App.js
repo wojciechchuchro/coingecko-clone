9a232208a77ec9b24f825516258a0a78db9eb410
Wojciech Chuchro <wojciech.chuchro@outlook.com>
2022-08-27 18:19:28 +0200

    add logo to a header, delete unnecessary files

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import CryptoDetail from "./pages/CryptoDetail";
 import CryptoHome from "./pages/CryptoHome";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <BrowserRouter>
-      Navbar
+      <NavBar />
       <Routes>
         <Route path="/" element={<CryptoHome />} />
         <Route path="/coin/:id" element={<CryptoDetail />} />
